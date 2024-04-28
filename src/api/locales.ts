0d20100631b06ec1fe5372f27b44118486e28b75
nurxan <ertaev.nurxan@bk.ru>
2024-04-29 01:19:18 +0600

    updated api path

--- src/api/locales.ts
+++ src/api/locales.ts
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-const API_URL = "http://localhost:8000/locales";
+const API_URL = "https://fake-users-backend-bdf0d6c4d589.herokuapp.com";
 
 export interface Locale {
   name: string;
